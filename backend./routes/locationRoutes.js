5f62bf5a23c6b87fcb169c4d4306ade5553f5d43
Spec.K <winstonkoby2@gmail.com>
2026-04-22 18:59:44 -0500

    Add location routes for user saved locations

---
+++ backend./routes/locationRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import auth from "../middleware/authMiddleware.js";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+router.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user.id);
+  res.json(user.savedLocations || []);
+});
+
+router.post("/", auth, async (req, res) => {
+  const { label, type, location } = req.body;
+  const user = await User.findById(req.user.id);
+  user.savedLocations.push({ label, type, location });
+  await user.save();
+  res.json(user.savedLocations);
+});
+
+router.delete("/:index", auth, async (req, res) => {
+  const user = await User.findById(req.user.id);
+  user.savedLocations.splice(req.params.index, 1);
+  await user.save();
+  res.json(user.savedLocations);
+});
+
+export default router;
