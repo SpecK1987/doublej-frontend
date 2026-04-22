128f352e2aaf1444ad07372711a8cb2f1e5d7e06
Spec.K <winstonkoby2@gmail.com>
2026-04-21 19:03:26 -0500

    Update Login.tsx

--- frontend/pages/admin/Login.tsx
+++ frontend/pages/admin/Login.tsx
@@ -1,2 +1,4 @@
 // Same as customer login but redirects to /admin/orders
 window.location.href = "/admin/orders";
+localStorage.setItem("token", data.token);
+localStorage.setItem("role", data.user.role);
