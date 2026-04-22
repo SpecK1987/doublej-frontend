25bc15de1e7be597c93c3f981d1b169f76f974e9
Spec.K <winstonkoby2@gmail.com>
2026-04-21 19:09:24 -0500

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import PortalLogin from "./pages/portal/Login";
+import Orders from "./pages/portal/Orders";
+import NewOrder from "./pages/portal/NewOrder";
+import AdminLogin from "./pages/admin/Login";
+import AdminOrders from "./pages/admin/Orders";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+
+          <Route path="/portal/login" element={<PortalLogin />} />
+          <Route
+            path="/portal/orders"
+            element={
+              <ProtectedRoute>
+                <Orders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/portal/orders/new"
+            element={
+              <ProtectedRoute>
+                <NewOrder />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route
+            path="/admin/orders"
+            element={
+              <ProtectedRoute requireAdmin>
+                <AdminOrders />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  </React.StrictMode>
+);
