5c83d3c8c2ab6f9ff7235640ac462d152d0fa40b
Spec.K <winstonkoby2@gmail.com>
2026-04-22 11:30:06 -0500

    Update User.js

--- backend./models/User.js
+++ backend./models/User.js
@@ -1,10 +1,16 @@
-import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  role: { type: String, default: "customer" }
+  role: { type: String, default: "customer" },
+  company: String,
+  phone: String,
+  defaultInstructions: String,
+  savedLocations: [
+    {
+      label: String,
+      type: String, // "Boat" | "Platform" | "Business"
+      location: String,
+    },
+  ],
 });
-
-export default mongoose.model("User", userSchema);
