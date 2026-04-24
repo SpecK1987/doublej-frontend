e8df0a407e3dbe4834536578c032da24dc0c7f7f
Spec.K <winstonkoby2@gmail.com>
2026-04-24 11:40:18 -0500

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F6B800",
+        navy: "#001B3A",
+        lightgrey: "#F3F4F6"
+      }
+    }
+  },
+  plugins: []
+};
