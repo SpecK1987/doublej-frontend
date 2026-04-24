a25fcfa6b793a44864352eee27f7964c467131a1
Spec.K <winstonkoby2@gmail.com>
2026-04-24 16:16:28 -0500

    Delete frontend/src/pages/portal/Neworder.tsx

--- frontend/src/pages/portal/Neworder.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-import { API, authHeader } from "../../utils/api";
-
-export default function NewOrder() {
-  const [step, setStep] = useState(1);
-
-  const [pickupLocation, setPickup] = useState("");
-  const [deliveryLocation, setDelivery] = useState("");
-  const [goodsType, setGoodsType] =
