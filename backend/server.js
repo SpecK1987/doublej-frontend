773e08f1bb5d28147d932669e07d240b80746b29
Spec.K <winstonkoby2@gmail.com>
2026-04-26 08:53:52 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import orderRoutes from "./routes/orders.js";
+
+app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
 
 dotenv.config();
 
