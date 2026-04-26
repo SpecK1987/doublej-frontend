e87b1ff20f1ff68aaeb64a20d2f960db0056ac83
Spec.K <winstonkoby2@gmail.com>
2026-04-26 18:38:48 -0500

    Refactor import statements in locationRoutes.js

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import auth from "../middleware/authMiddleware.js";
+import { protect, adminOnly } from "../middleware/authMiddleware.js";
 import User from "../models/User.js";
 
 const router = express.Router();
