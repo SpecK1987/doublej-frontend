b83566e62f8e6392ffe62ce003360568d60d1038
Spec.K <winstonkoby2@gmail.com>
2026-04-26 18:11:42 -0500

    Change server port from 10000 to 5000

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,5 @@ app.get("/", (req, res) => {
 });
 
 // Port
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
