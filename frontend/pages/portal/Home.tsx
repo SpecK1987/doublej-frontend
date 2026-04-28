bcd0d8c20d262efa8053c903bc98a102ee21d5ec
Spec.K <winstonkoby2@gmail.com>
2026-04-28 14:23:27 -0500

    Update Home.tsx

--- frontend/pages/portal/Home.tsx
+++ frontend/pages/portal/Home.tsx
@@ -3,7 +3,11 @@ import PortalLayout from "../../components/PortalLayout";
 const Home = () => {
   return (
     <PortalLayout>
-      <h1 className="text-2xl font-bold">Welcome to your dashboard</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to Your Dashboard</h1>
+      <p className="text-gray-700">
+        From here you can create new service orders, view your existing orders,
+        manage saved locations, and update your profile.
+      </p>
     </PortalLayout>
   );
 };
