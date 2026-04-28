a474676754b0d993ec51036c176820b2874baa24
Spec.K <winstonkoby2@gmail.com>
2026-04-28 14:18:01 -0500

    Add Home component for portal dashboard

---
+++ frontend/pages/portal/Home.tsx
@@ -0,0 +1,11 @@
+import PortalLayout from "../../components/PortalLayout";
+
+const Home = () => {
+  return (
+    <PortalLayout>
+      <h1 className="text-2xl font-bold">Welcome to your dashboard</h1>
+    </PortalLayout>
+  );
+};
+
+export default Home;
