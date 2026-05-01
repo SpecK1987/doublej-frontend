359782c1c28638d281f0586bf811c1716516d14a
Spec.K <winstonkoby2@gmail.com>
2026-05-01 13:53:32 -0500

    Refactor App component to accept children

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,72 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import FAQ from "./pages/FAQ";
-
-import Login from "./pages/portal/Login";
-import NewOrder from "./pages/portal/NewOrder";
-import Orders from "./pages/portal/Orders";
-import SavedLocations from "./pages/portal/SavedLocations";
-import Profile from "./pages/portal/Profile";
-
-import ProtectedRoute from "./components/ProtectedRoute";
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Website */}
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/faq" element={<FAQ />} />
-
-        {/* Auth */}
-        <Route path="/portal/login" element={<Login />} />
-
-        {/* Portal Protected Routes */}
-        <Route
-          path="/portal/new-order"
-          element={
-            <ProtectedRoute>
-              <NewOrder />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/portal/orders"
-          element={
-            <ProtectedRoute>
-              <Orders />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/portal/locations"
-          element={
-            <ProtectedRoute>
-              <SavedLocations />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/portal/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+const App = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
 };
 
 export default App;
