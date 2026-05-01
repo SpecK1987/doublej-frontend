7d3eff7581eedda6270b792583d1d60682efd916
Spec.K <winstonkoby2@gmail.com>
2026-05-01 03:28:14 -0500

    Refactor routing structure and add portal/admin routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,83 +1,106 @@
-import App from "./App";
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
+import App from "./App";
+import "./index.css";
+
+// Public pages
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import FAQ from "./pages/FAQ";
 
-import PortalLogin from "./pages/portal/Login";
+// Portal pages
+import Login from "./pages/portal/Login";
 import Orders from "./pages/portal/Orders";
 import NewOrder from "./pages/portal/NewOrder";
 import Profile from "./pages/portal/Profile";
 import SavedLocations from "./pages/portal/SavedLocations";
 
+// Admin pages
+import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminOrders from "./pages/admin/Orders";
 
+// Route protection
 import ProtectedRoute from "./components/ProtectedRoute";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/faq" element={<FAQ />} />
+        <Routes>
+          {/* Public Website */}
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/faq" element={<FAQ />} />
 
-            <Route path="/portal/login" element={<PortalLogin />} />
-            <Route
-              path="/portal/orders"
-              element={
-                <ProtectedRoute>
-                  <Orders />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/portal/orders/new"
-              element={
-                <ProtectedRoute>
-                  <NewOrder />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/portal/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/portal/locations"
-              element={
-                <ProtectedRoute>
-                  <SavedLocations />
-                </ProtectedRoute>
-              }
-            />
+          {/* Portal Auth */}
+          <Route path="/portal/login" element={<Login />} />
 
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route
-              path="/admin/orders"
-              element={
-                <ProtectedRoute requireAdmin>
-                  <AdminOrders />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </App>
+          {/* Portal Protected Routes */}
+          <Route
+            path="/portal/orders"
+            element={
+              <ProtectedRoute>
+                <Orders />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/portal/orders/new"
+            element={
+              <ProtectedRoute>
+                <NewOrder />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/portal/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/portal/locations"
+            element={
+              <ProtectedRoute>
+                <SavedLocations />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Admin Routes */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute requireAdmin>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin/orders"
+            element={
+              <ProtectedRoute requireAdmin>
+                <AdminOrders />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Fallback */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
