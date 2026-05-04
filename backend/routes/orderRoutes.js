94d4c971d633f83cb179ae23cd6b660b62cc9c22
Spec.K <winstonkoby2@gmail.com>
2026-05-03 19:11:58 -0500

    Change Order import to named import

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Order from "../models/Order.js";
+import { Order } from "../models/Order.js";
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
